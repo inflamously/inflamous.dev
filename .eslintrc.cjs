4ba97211df0421affe8b223499bd5e6434b6c301
Inflamously <david.dembinski.1996@gmail.com>
2024-04-28 01:53:37 +0200

    fix: some package issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,79 +6,82 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
+    // Custom nflamously.dev rules
+    rules: {
+        "no-unused-vars": "off"
     },
-  },
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-  },
-  ignorePatterns: ["!**/.server", "!**/.client"],
-
-  // Base config
-  extends: ["eslint:recommended"],
-
-  overrides: [
-    // React
-    {
-      files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
-      extends: [
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-      ],
-      settings: {
-        react: {
-          version: "detect",
+    root: true,
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
         },
-        formComponents: ["Form"],
-        linkComponents: [
-          { name: "Link", linkAttribute: "to" },
-          { name: "NavLink", linkAttribute: "to" },
-        ],
-        "import/resolver": {
-          typescript: {},
-        },
-      },
     },
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+    },
+    ignorePatterns: ["!**/.server", "!**/.client", 'server.js'],
+    // Base config
+    extends: ["eslint:recommended"],
 
-    // Typescript
-    {
-      files: ["**/*.{ts,tsx}"],
-      plugins: ["@typescript-eslint", "import"],
-      parser: "@typescript-eslint/parser",
-      settings: {
-        "import/internal-regex": "^~/",
-        "import/resolver": {
-          node: {
-            extensions: [".ts", ".tsx"],
-          },
-          typescript: {
-            alwaysTryTypes: true,
-          },
+    overrides: [
+        // React
+        {
+            files: ["**/*.{js,jsx,ts,tsx}"],
+            plugins: ["react", "jsx-a11y"],
+            extends: [
+                "plugin:react/recommended",
+                "plugin:react/jsx-runtime",
+                "plugin:react-hooks/recommended",
+                "plugin:jsx-a11y/recommended",
+            ],
+            settings: {
+                react: {
+                    version: "detect",
+                },
+                formComponents: ["Form"],
+                linkComponents: [
+                    {name: "Link", linkAttribute: "to"},
+                    {name: "NavLink", linkAttribute: "to"},
+                ],
+                "import/resolver": {
+                    typescript: {},
+                },
+            },
         },
-      },
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-      ],
-    },
 
-    // Node
-    {
-      files: [".eslintrc.cjs"],
-      env: {
-        node: true,
-      },
-    },
-  ],
+        // Typescript
+        {
+            files: ["**/*.{ts,tsx}"],
+            plugins: ["@typescript-eslint", "import"],
+            parser: "@typescript-eslint/parser",
+            settings: {
+                "import/internal-regex": "^~/",
+                "import/resolver": {
+                    node: {
+                        extensions: [".ts", ".tsx"],
+                    },
+                    typescript: {
+                        alwaysTryTypes: true,
+                    },
+                },
+            },
+            extends: [
+                "plugin:@typescript-eslint/recommended",
+                "plugin:import/recommended",
+                "plugin:import/typescript",
+            ],
+        },
+
+        // Node
+        {
+            files: [".eslintrc.cjs"],
+            env: {
+                node: true,
+            },
+        },
+    ],
 };
